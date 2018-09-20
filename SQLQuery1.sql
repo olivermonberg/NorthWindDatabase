37b2789a09df6524be35280c454f5ee0f5be1701
Oliver Monberg <au554208@uni.au.dk>
2018-09-20 10:35:51 +0200

    Simple queries

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,6 +6,17 @@
 
 --SELECT * FROM Customers WHERE Country='UK'
 
-SELECT * FROM [Order Details]
+--SELECT * FROM [Order Details]
 
 --SELECT * FROM [Order Details] HAVING COUNT (Quantity) >=10 AND COUNT (Quantity) <=20
+
+--SELECT LastName, FirstName, BirthDate, City, Country, Title FROM Employees WHERE Title LIKE '%Sales%'
+
+--SELECT CustomerID, CompanyName, City, PostalCode FROM Customers WHERE City='London' ORDER BY PostalCode
+
+--SELECT ContactName, ContactTitle, Country, CompanyName FROM Customers
+--WHERE (Country='USA' OR Country='Spain') AND ContactTitle LIKE '%Marketing Manager%' ORDER BY CompanyName DESC
+
+SELECT ContactName
+FROM Customers 
+WHERE (Country='USA' OR Country='Spain' OR Country='UK') AND Fax IS null
