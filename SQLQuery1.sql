f86c37c0ca2deb29faa7dd0243576f024228cdb5
Oliver Monberg <au554208@uni.au.dk>
2018-09-20 14:09:14 +0200

    ny

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -17,6 +17,19 @@
 --SELECT ContactName, ContactTitle, Country, CompanyName FROM Customers
 --WHERE (Country='USA' OR Country='Spain') AND ContactTitle LIKE '%Marketing Manager%' ORDER BY CompanyName DESC
 
-SELECT ContactName
-FROM Customers 
-WHERE (Country='USA' OR Country='Spain' OR Country='UK') AND Fax IS null
+--SELECT ContactName
+--FROM Customers 
+--WHERE (Country='USA' OR Country='Spain' OR Country='UK') AND Fax IS null
+
+--SELECT count (*) AS [Number of Customers] FROM Customers
+
+--SELECT * FROM [Order Details]
+
+--Opg 14
+--SELECT Country AS Land , COUNT(Country) as [Antal kunder i landet] FROM Customers GROUP BY Country
+
+--SELECT Country AS Land , COUNT(Country) AS [Antal kunder i landet] FROM Customers 
+--GROUP BY Country HAVING COUNT(Country) >=2
+
+--Opg 19 not working
+--SELECT FirstName, LastName FROM Employees WHERE Region LIKE '%Southern%'
